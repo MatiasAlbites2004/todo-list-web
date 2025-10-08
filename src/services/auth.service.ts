fd19e2fca68f8cc291a971f219c3c7cf3344eda5
MatiasAlbites2004 <matiasalbites1512@gmail.com>
2025-10-07 19:20:37 -0500

    Feat: Integration Login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,13 +1,7 @@
-export const registerUser = async (data: {
-  name: string;
-  email: string;
-  password: string;
-}) => {
+export const registerUser = async (data: { name: string; email: string; password: string }) => {
   const res = await fetch("http://localhost:3000/auth/register", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
 
@@ -16,5 +10,20 @@ export const registerUser = async (data: {
     throw new Error(errorData.error || "Error al registrar usuario");
   }
 
-  return res.json(); 
+  return res.json();
+};
+
+export const loginUser = async (data: { email: string; password: string }) => {
+  const res = await fetch("http://localhost:3000/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.error || "Error al iniciar sesión");
+  }
+
+  return res.json();
 };
